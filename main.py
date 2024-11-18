49e83fbf382f3f773bd0aa58afb6d5bc9399b391
Bezrukov47 <bezr47@gmail.com>
2024-11-18 18:26:32 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,28 @@
+my_dict = {'Kosta': 1947, 'Zahar': 1984}
+print(my_dict)
+print(my_dict['Kosta'])
+print(my_dict.get('Anna'))
+my_dict.update ({'Katia': 2019, 'Vera': 2019})
+print (my_dict)
+print(my_dict['Vera'])
+print(my_dict)
+my_dict.pop('Vera')
+print(my_dict)
+my_set_={1,2,3,1, 2, 4, 3, 5, 'John', 'John'}
+print(my_set_)
+my_set_.update ({'Lera', 'Vera'})
+print (my_set_)
+my_set_.discard('Lera')
+print(my_set_)
+
+
+
+
+
+
+
+
+
+
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
